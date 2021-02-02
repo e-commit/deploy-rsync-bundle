8e3c9a0049000366a9778236d7fce733ff0b6399
hlecorche <hlecorche@e-commit.fr>
2021-02-02 17:39:58 +0100

    Fix extension name

--- src/DependencyInjection/EcommitDeployRsyncExtension.php
+++ src/DependencyInjection/EcommitDeployRsyncExtension.php
@@ -18,7 +18,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-class EcommitDeployRsyncBundleExtension extends Extension
+class EcommitDeployRsyncExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {

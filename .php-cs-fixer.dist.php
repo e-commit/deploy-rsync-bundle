613ef6f4dd8b85ce2a6357df587d667c3a6e7a6a
hlecorche <hlecorche@e-commit.fr>
2022-03-05 11:43:18 +0100

    Upgrade php-cs-fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -13,7 +13,7 @@ $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__)
 ;
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
         '@Symfony' => true,

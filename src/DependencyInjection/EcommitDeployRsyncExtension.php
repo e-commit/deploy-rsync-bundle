f27e959cdc33eb5d2acbb8216270cae2186803c9
hlecorche <hlecorche@e-commit.fr>
2022-04-10 20:11:55 +0200

    New bundle structure

--- src/DependencyInjection/EcommitDeployRsyncExtension.php
+++ src/DependencyInjection/EcommitDeployRsyncExtension.php
@@ -25,7 +25,7 @@ class EcommitDeployRsyncExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
         $loader->load('services.xml');
 
         $container->setParameter('ecommit_deploy_rsync.environments', $config['environments']);
